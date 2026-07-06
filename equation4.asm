757702647f3c13f095b3e4b1af3fc7789174c5c6
Fuge03 <85267954+Fuge03@users.noreply.github.com>
2026-07-06 04:01:57 +0000

    Completed the assembly code for all 4 equations

---
+++ equation4.asm
@@ -0,0 +1,23 @@
+section .text
+global _start
+
+_start:
+    mov eax, [var1]
+    mov ebx, 2
+    mul ebx
+    
+    mov ebx, [var2]
+    sub ebx, 3
+    
+    div ebx
+    mov [result], eax
+
+    mov eax, 1
+    int 0x80
+
+section .data
+    var1 dd 10
+    var2 dd 7
+
+section .bss
+    result resd 1
